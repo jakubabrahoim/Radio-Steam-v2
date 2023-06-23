cc2f0e12e5c04d213e61bce580b53e388e3f6976
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2023-06-23 23:15:36 +0200

    add bouncing chevron with scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+'use client';
+
 import MediaIllustration from '@/components/media-illustration';
+import { ChevronDown } from 'lucide-react';
 import { Dela_Gothic_One } from 'next/font/google';
-import Image from 'next/image';
+import { useRef } from 'react';
 
 const gothic = Dela_Gothic_One({ weight: '400', subsets: ['latin'] });
 
 export default function Home() {
+    const section2Ref = useRef<HTMLDivElement>(null);
+
+    const scrollToSection2 = () => {
+        section2Ref.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
         <main className='flex min-h-screen flex-col items-center'>
-            <section className='flex h-screen w-screen items-center justify-center bg-dark-blue-200'>
+            <section className='relative flex h-screen w-screen items-center justify-center bg-dark-blue-200'>
                 <div className='mx-auto flex w-full max-w-6xl  items-center justify-between'>
                     <div className='flex flex-col items-start justify-center gap-10'>
                         <button className='rounded-full border border-gold bg-transparent px-4 py-2 font-semibold text-gold'>
@@ -30,9 +39,19 @@ export default function Home() {
                         />
                     </div>
                 </div>
+
+                <button
+                    onClick={scrollToSection2}
+                    className='absolute inset-x-auto bottom-2'
+                >
+                    <ChevronDown className='h-10 w-10 animate-bounce text-white' />
+                </button>
             </section>
 
-            <section className='flex h-screen w-screen items-center justify-center bg-white'>
+            <section
+                ref={section2Ref}
+                className='flex h-screen w-screen items-center justify-center bg-white'
+            >
                 <div className='mx-auto flex w-full max-w-6xl  items-center justify-between'>
                     <h1
                         className={`max-w-2xl text-start text-6xl font-bold ${gothic.className}`}
