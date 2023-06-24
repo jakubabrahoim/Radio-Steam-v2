9e9bf2fb326280f8362547164e914524bb66ade3
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2023-06-24 11:10:23 +0200

    update tailwind colors

--- app/page.tsx
+++ app/page.tsx
@@ -16,20 +16,20 @@ export default function Home() {
 
     return (
         <main className='flex min-h-screen flex-col items-center'>
-            <section className='relative flex h-screen w-screen items-center justify-center bg-dark-blue-200'>
+            <section className='relative flex h-screen w-screen items-center justify-center bg-cream-200'>
                 <div className='mx-auto flex w-full max-w-6xl  items-center justify-between'>
                     <div className='flex flex-col items-start justify-center gap-10'>
-                        <button className='rounded-full border border-gold bg-transparent px-4 py-2 font-semibold text-gold'>
+                        <span className='rounded-full border border-fuchsia-900 bg-transparent px-4 py-2 font-semibold text-fuchsia-900'>
                             Stream World&apos;s Music
-                        </button>
+                        </span>
                         <h1
-                            className={`max-w-sm text-start text-6xl font-bold text-gold ${gothic.className}`}
+                            className={`max-w-sm text-start text-6xl font-bold text-fuchsia-900 ${gothic.className}`}
                         >
                             Tune in to the globe
                         </h1>
                         <button
                             onClick={scrollToSection2}
-                            className='rounded-full bg-cream px-4 py-2 font-semibold text-fuchsia'
+                            className='rounded-full border border-cream-300 bg-cream-100 px-4 py-2 font-semibold text-black transition-transform hover:scale-105 hover:shadow-sm'
                         >
                             Discover now
                         </button>
@@ -47,13 +47,13 @@ export default function Home() {
                     onClick={scrollToSection2}
                     className='absolute inset-x-auto bottom-2'
                 >
-                    <ChevronDown className='h-10 w-10 animate-bounce text-white' />
+                    <ChevronDown className='h-10 w-10 animate-bounce text-fuchsia-900' />
                 </button>
             </section>
 
             <section
                 ref={section2Ref}
-                className='flex h-screen w-screen items-center justify-center bg-white'
+                className='flex h-screen w-screen items-center justify-center bg-cream-100'
             >
                 <div className='mx-auto flex w-full max-w-6xl  items-center justify-between'>
                     <h1
