eed93bd73a061a0fadf0835966be13132bab9fb0
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2023-06-24 11:46:03 +0200

    add features section to lp

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,14 @@ const gothic = Dela_Gothic_One({ weight: '400', subsets: ['latin'] });
 
 export default function Home() {
     const section2Ref = useRef<HTMLDivElement>(null);
+    const section3Ref = useRef<HTMLDivElement>(null);
 
     const scrollToSection2 = () => {
         section2Ref.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const scrollToSection3 = () => {
+        section3Ref.current?.scrollIntoView({ behavior: 'smooth' });
     };
 
     return (
@@ -53,7 +58,7 @@ export default function Home() {
 
             <section
                 ref={section2Ref}
-                className='flex h-screen w-screen items-center justify-center bg-cream-100'
+                className='relative flex h-screen w-screen items-center justify-center bg-cream-100'
             >
                 <div className='mx-auto flex w-full max-w-6xl  items-center justify-between'>
                     <h1
@@ -62,6 +67,73 @@ export default function Home() {
                         Explore diverse stations from around the world.
                     </h1>
                     <MediaIllustration />
+                </div>
+
+                <button
+                    onClick={scrollToSection3}
+                    className='absolute inset-x-auto bottom-2'
+                >
+                    <ChevronDown className='h-10 w-10 animate-bounce text-fuchsia-900' />
+                </button>
+            </section>
+
+            <section
+                ref={section3Ref}
+                className='flex h-screen w-screen items-center justify-center bg-cream-200'
+            >
+                <div className='mx-auto grid w-full max-w-6xl  grid-cols-3 items-center  justify-between gap-6'>
+                    <div className='col-span-2 rounded-2xl bg-cream-50 p-8'>
+                        <h2
+                            className={`text-2xl font-bold text-fuchsia-900 ${gothic.className}`}
+                        >
+                            Likes
+                        </h2>
+                        <span>Save your favorite stations</span>
+                        <img
+                            className='mt-5 h-60 w-full rounded-lg object-cover grayscale'
+                            src='/images/landing-likes.png'
+                            alt='likes'
+                        />
+                    </div>
+                    <div className='col-span-1 rounded-2xl bg-cream-50 p-8'>
+                        <h2
+                            className={`text-2xl font-bold text-fuchsia-900 ${gothic.className}`}
+                        >
+                            Search
+                        </h2>
+                        <span>Find stations easily</span>
+                        <img
+                            className='mt-5 h-60 w-full rounded-lg object-cover grayscale'
+                            src='/images/landing-search.png'
+                            alt='search'
+                        />
+                    </div>
+                    <div className='col-span 1 rounded-2xl bg-cream-50 p-8'>
+                        <h2
+                            className={`text-2xl font-bold text-fuchsia-900 ${gothic.className}`}
+                        >
+                            Categories
+                        </h2>
+                        <span>Browse by genre</span>
+                        <img
+                            className='mt-5 h-60 w-full rounded-lg object-cover grayscale'
+                            src='/images/landing-categories.png'
+                            alt='categories'
+                        />
+                    </div>
+                    <div className='col-span-2 rounded-2xl bg-cream-50 p-8'>
+                        <h2
+                            className={`text-2xl font-bold text-fuchsia-900 ${gothic.className}`}
+                        >
+                            Countries
+                        </h2>
+                        <span>Discover radio stations worldwide</span>
+                        <img
+                            className='mt-5 h-60 w-full rounded-lg object-cover grayscale'
+                            src='/images/landing-countries.png'
+                            alt='countries'
+                        />
+                    </div>
                 </div>
             </section>
         </main>
