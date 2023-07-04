3db7d8d1e8df7453fc61e76f966d5e16a0d0cb42
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2023-07-04 13:28:45 +0200

    audio player ui

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,7 +1,31 @@
+import { Play, Radio } from 'lucide-react';
+import { Slider } from '@/components/ui/slider';
+
 export default function AudioPlayer() {
     return (
-        <div className='h-full w-full border-t border-cream-100'>
-            Audio player
+        <div className='flex h-full w-full items-center justify-between border-t border-cream-100 px-4 py-2'>
+            <section className='flex items-center justify-center gap-2'>
+                <div className='flex items-center justify-center rounded-lg border border-cream-200 bg-cream-100 p-3'>
+                    <Radio className='h-6 w-6' />
+                </div>
+                Station name
+            </section>
+
+            <section>
+                <button className='flex items-center justify-center rounded-full bg-fuchsia-900 p-3'>
+                    <Play fill='white' className='ml-0.5 h-5 w-5 text-white' />
+                </button>
+            </section>
+
+            <section className='flex flex-col gap-2 self-start'>
+                Volume
+                <Slider
+                    className='w-52'
+                    defaultValue={[50]}
+                    max={100}
+                    step={1}
+                />
+            </section>
         </div>
     );
 }
--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -4,7 +4,12 @@ import SearchMenu from '@/components/SearchMenu';
 
 export default function TopBar() {
     return (
-        <div className='flex h-full w-full items-center justify-end border border-cream-100 p-2'>
+        <div className='flex h-full w-full items-center justify-between border border-cream-100 p-2'>
+            <section>
+                <span className='pointer-events-none text-sm font-bold'>
+                    Radio Stream
+                </span>
+            </section>
             <SearchMenu />
         </div>
     );
